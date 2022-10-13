41d24b87350883ffad2b5f2f72d40ea898c878c9
Jhon Tajumbina <jhonse.tajumbina@umariana.edu.co>
2022-10-12 19:00:15 -0500

    Se termino el ejercicio , esta funcionando

--- Datos.py
+++ Datos.py
@@ -2,5 +2,15 @@ name=input("what it´s your name? ")
 
 old=int(input("how many years old?"))
 
+mayorDeEdad= old>18
 
+respuestaHijoDueño=input("eres el hijo del dueño?")
+hijoDueño=respuestaHijoDueño=='si'
+
+
+dejarPasar=mayorDeEdad or hijoDueño
+if dejarPasar:
+     print("Bienvenido puedes ingresar")
+else:
+    print('Lo siento no puedes ingresar')
 
